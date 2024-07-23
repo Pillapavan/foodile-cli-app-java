075cce29b0845d0a0d0794a097adefc097144531
pillapavan <pillapavan90909@gmail.com>
2024-07-23 21:39:12 +0530

    feat: Add Model Classes Customer,Dish,Order,Restaurant

---
+++ src/com/java/foodiecli/model/Restaurant.java
@@ -0,0 +1,71 @@
+package com.java.foodiecli.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Restaurant {
+    // String id, String name, String address,List<Dish> menu
+    private String id;
+    private String name;
+    private String address;
+    private List<Dish> menu;
+
+    Restaurant(){
+
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<Dish> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(List<Dish> menu) {
+        this.menu = menu;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Restaurant that = (Restaurant) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(address, that.address) && Objects.equals(menu, that.menu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, address, menu);
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "address='" + address + '\'' +
+                ", id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", menu=" + menu +
+                '}';
+    }
+}
