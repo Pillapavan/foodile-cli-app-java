60bee1a8b6ddc4e2ccd63c046c3d4fa2eef5f30e
pillapavan <pillapavan90909@gmail.com>
2024-07-24 19:48:52 +0530

    feat: Add CustomerRepository

---
+++ src/com/java/foodiecli/repository/CustomerRepository.java
@@ -0,0 +1,23 @@
+package com.java.foodiecli.repository;
+
+import com.java.foodiecli.model.Customer;
+import com.java.foodiecli.util.CsvReader;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerRepository {
+    private List<Customer> customerList;
+
+    public CustomerRepository(){
+        CsvReader csvReader=new CsvReader();
+        this.customerList=csvReader.readCustomerFromCsv();
+    }
+    public Customer saveCustomer(Customer customer){
+        this.customerList.add(customer);
+        return customer;
+    }
+    public Optional<Customer> getCustomerById(String id){
+        return  this.customerList.stream().filter(customer -> customer.getId().equals(id)).findFirst();
+    }
+}
