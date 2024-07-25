b36188fd7d38969f22e883f21e0f32e7cc8d44a0
pillapavan <pillapavan90909@gmail.com>
2024-07-25 21:15:00 +0530

    feat:Add RestaurantNotFoundException

---
+++ src/com/java/foodiecli/exceptions/RestaurantNotFoundException.java
@@ -0,0 +1,7 @@
+package com.java.foodiecli.exceptions;
+
+public class RestaurantNotFoundException extends Exception{
+    public RestaurantNotFoundException(String message) {
+        super(message);
+    }
+}
