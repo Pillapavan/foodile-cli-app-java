b9c510b4bb5c15659fef0db88872e76c43761328
pillapavan <pillapavan90909@gmail.com>
2024-07-28 09:37:15 +0530

    feat: Add Validations

---
+++ src/com/java/foodiecli/validation/InputValidator.java
@@ -0,0 +1,29 @@
+package com.java.foodiecli.validation;
+
+import com.java.foodiecli.ui.Menu;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputValidator {
+    public int getValidMenuChoice(){
+        Scanner sc = new Scanner(System.in);
+        int input=0;
+        boolean vaildInput=false;
+        while (!vaildInput){
+            try {
+                input= sc.nextInt();
+                if (input>=1 && input<=5){
+                    vaildInput=true;
+                }
+                else {
+                    System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+                ;
+            }
+        }
+        return input;
+    }
+}
