af9d05c56df2aab519f9401ab24081f043151164
pillapavan <pillapavan90909@gmail.com>
2024-07-28 09:32:22 +0530

    feat: Add Exceptions for Customer CRUD

---
+++ src/com/java/foodiecli/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.java.foodiecli.exceptions;
+
+public class CustomerNotFoundException extends Exception{
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
