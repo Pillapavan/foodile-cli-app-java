905b768cee1b3e2b279da780ff04aafe83214cbc
pillapavan <pillapavan90909@gmail.com>
2024-07-28 09:30:08 +0530

    refactor: Implemented Cusotmer CRUD methods

--- src/com/java/foodiecli/controller/CustomerController.java
+++ src/com/java/foodiecli/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.java.foodiecli.controller;
 
 import com.java.foodiecli.exceptions.CustomerAlreadyExistsException;
+import com.java.foodiecli.exceptions.CustomerNotFoundException;
 import com.java.foodiecli.model.Customer;
 import com.java.foodiecli.service.CustomerService;
 import com.java.foodiecli.service.CustomerServiceImpl;
+
+import java.util.List;
 
 public class CustomerController {
     private CustomerServiceImpl customerServiceImpl;
@@ -14,5 +17,20 @@ public class CustomerController {
 
     public Customer save(Customer customer) throws CustomerAlreadyExistsException{
         return this.customerServiceImpl.save(customer);
+    }
+    public Customer validateCustomerLogin(String Email,String passWord) throws CustomerNotFoundException{
+        return this.customerServiceImpl.validateCustomerLogin(Email,passWord);
+    }
+    public Customer getCustomerById(String Id) throws CustomerNotFoundException{
+        return  this.customerServiceImpl.getCustomerById(Id);
+    }
+    public List<Customer> getAllCustomers(){
+        return this.customerServiceImpl.getAllCustomers();
+    }
+    public Customer updateCustomer(Customer customer) throws CustomerNotFoundException{
+        return this.customerServiceImpl.updateCustomer(customer);
+    }
+    public void deleteCustomer(String id) throws CustomerNotFoundException{
+        this.customerServiceImpl.deleteCustomer(id);
     }
 }
