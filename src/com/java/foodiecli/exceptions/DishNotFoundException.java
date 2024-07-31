2602939297ab807ac3f56ed3895fe9f9b318d5b7
pillapavan <pillapavan90909@gmail.com>
2024-07-31 22:27:57 +0530

    feat: Add Exceptions for Dish CRUD

---
+++ src/com/java/foodiecli/exceptions/DishNotFoundException.java
@@ -0,0 +1,7 @@
+package com.java.foodiecli.exceptions;
+
+public class DishNotFoundException extends Exception{
+    public DishNotFoundException(String message) {
+        super(message);
+    }
+}
